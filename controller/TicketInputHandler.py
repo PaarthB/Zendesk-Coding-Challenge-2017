f8e3f5de4585e4731128aac9b43c5d83b68106e9
Paarth Bhasin <paarthbhasin4@gmail.com>
2017-07-11 22:11:17 +1000

    Add files via upload

---
+++ controller/TicketInputHandler.py
@@ -0,0 +1,7 @@
+from view.GUI import GUI
+
+
+class TicketInputHandler:
+    def __init__(self):
+        self.view = GUI(2)
+
---
+++ view/GUI.py
@@ -0,0 +1,4 @@
+class GUI:
+    i = 0
+    def __init__(self, salary):
+        GUI.i = salary
