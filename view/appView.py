6c3a9342634cbdad8be4b6be669893a49232f4c7
Paarth Bhasin <paarthbhasin4@gmail.com>
2017-07-18 18:45:24 +1000

    Added print functionality from controller & model
    Made view do 99.9% of printing messages and information, except one odd message that still has to be displayed by the model.

--- view/appView.py
+++ view/appView.py
@@ -45,6 +45,18 @@ class AppView:
         print("Page command error. Enter 'd' for down, 'u' for up, 'menu' for menu and 3 for quit")
         return 1
 
+    def unknownError(self):
+        print("Either no tickets on account or unknown error occurred")
+        return 1
+
+    def authenticationError(self):
+        print("Authentication not permitted or invalid user credentials.")
+        return 1
+
+    def apiUnavailable(self):
+        print("API unavailable. Please try again later")
+        return 1
+
     def displayTickets(self, ticketsJSON, pageNo):
         ticketsArr = ticketsJSON["tickets"]
         # rounding up ticket pages
@@ -62,25 +74,30 @@ class AppView:
                 if ticketsArr[i]["id"] is None:
                     continue
                 else:
-                    self.printTicket(ticketsArr[i]["id"], ticketsArr[i]["status"], ticketsArr[i]["subject"],
+                    self.printTicket(ticketsArr[i]["id"], ticketsArr[i]["status"], "",
                                      ticketsArr[i]["requester_id"], ticketsArr[i]["updated_at"])
                 pageTickets += 1
 
-        print("Displaying ", pageTickets, "tickets of page ", pageNo, " of ", totalPages)
+        print("Displaying", pageTickets, "tickets on page", pageNo, "of", totalPages)
         print("Enter 'd' for next page, 'u' for previous page, 'menu' for menu and q for quit")
         return pageNo  # Current page no
 
-    def displaySingleTicket(self, ticketsJSON):
+    def displaySingleTicket(self, ticketsJSON, ticketID=""):
+        print("Fetching ticket ", ticketID, ", please wait . . . . .")
         if "ticket" in ticketsJSON:
             self.printTicket(ticketsJSON["ticket"]["id"], ticketsJSON["ticket"]["status"],
-                             ticketsJSON["ticket"]["subject"],
-                             ticketsJSON["ticket"]["requester_id"], ticketsJSON["ticket"]["updated_at"])
+                             ticketsJSON["ticket"]["subject"], ticketsJSON["ticket"]["requester_id"],
+                             ticketsJSON["ticket"]["updated_at"])
             return 0
         else:
             return 1
 
-    def printTicket(self, ticketID, status, subject, requesterID, updatedAt):
-        print("<" + status + ">", "Ticket", str(ticketID), "subject '" + subject + "'", "opened by", requesterID,
-              "updated at", updatedAt)
+    def printTicket(self, ticketID, status, subject="", requesterID="", updatedAt=""):
+        if subject == "":
+            print("<" + status + ">", "Ticket", str(ticketID), "opened by", requesterID,
+                  "updated at", updatedAt)
+        else:
+            print("<" + status + ">", "Ticket", str(ticketID), "subject '" + subject + "'", "opened by", requesterID,
+                  "updated at", updatedAt)
 
         return 0
