6e44fa23dac420ce8d86bf4cbcf8a331adfdf564
Paarth Bhasin <paarthbhasin4@gmail.com>
2017-07-20 00:04:38 +1000

    Added more robust error handling

--- model/apiRequestHandler.py
+++ model/apiRequestHandler.py
@@ -12,7 +12,7 @@ class APIRequestHandler:
     def __init__(self):
         self.URL = ""
         self.data = {}  # This is where ticket data goes
-        self.subdomain = "parth"  # Zendesk API subdomain
+        self.subdomain = "paarth"  # Zendesk API subdomain
         self.loginID = "paarthbhasin4@gmail.com"  # Zendesk API username
         self.password = "Adprs123!"  # Zendesk API password
         self.errorCode = None
@@ -20,16 +20,18 @@ class APIRequestHandler:
     # Method to get all tickets in user's account and return them or return an appropriate error value
     def getTickets(self):
         ticketsJSON = self.connectToAPI(True, "")
-        if ticketsJSON in [1, False, None] or "tickets" not in ticketsJSON:
+        if ticketsJSON in [1, False, None, 0] or "tickets" not in ticketsJSON:
             if ticketsJSON is None:
                 return 1  # Invalid user credentials or authentication not enabled
             elif ticketsJSON == 1:
                 return 0  # If API is unavailable or other bad request
+            elif ticketsJSON == 0:
+                return False  # All other bad requests
             elif ticketsJSON is False or "tickets" not in ticketsJSON:
                 # For any other status code error or if no tickets exist
                 return -1
 
-        elif ticketsJSON not in [1, False, None] and "tickets" in ticketsJSON:
+        elif ticketsJSON not in [1, False, None, 0] and "tickets" in ticketsJSON:
             for i in range(len(ticketsJSON["tickets"])):
                 updated, created = self.formatDates(ticketsJSON["tickets"][i]["updated_at"],
                                                     ticketsJSON["tickets"][i]["created_at"])
@@ -40,19 +42,22 @@ class APIRequestHandler:
     # Method to get one ticket details from API and return it, or return appropriate error value
     def getTicket(self, ticketID):
         ticketsJSON = self.connectToAPI(False, ticketID)
-        if ticketsJSON not in [None, False, 1] and "ticket" in ticketsJSON:
+        if ticketsJSON not in [None, False, 1, 0] and "ticket" in ticketsJSON:
             updated, created = self.formatDates(ticketsJSON["ticket"]["updated_at"],
                                                 ticketsJSON["ticket"]["created_at"])
             ticketsJSON["ticket"]["updated_at"] = str(updated)
             ticketsJSON["ticket"]["created_at"] = str(created)
             return ticketsJSON
-        elif ticketsJSON in [None, False, 1]:
+        elif ticketsJSON in [None, False, 1, 0]:
             if ticketsJSON is False:
                 return -1  # Invalid ticket ID
             elif ticketsJSON == 1:
                 return 0  # If API is unavailable
             elif ticketsJSON is None:
                 return 1  # Invalid user credentials
+            elif ticketsJSON == 0:
+                return False  # All other bad requests
+            return False
 
     # Method to connect and query the Zendesk API to fetch tickets
     def connectToAPI(self, all=True, id=""):
@@ -72,7 +77,7 @@ class APIRequestHandler:
                     return False
                 elif r.status_code == 503:  # API unavailable
                     return 1
-                return 1  # For all other bad requests
+                return 0  # For all other bad requests
             self.data = r.json()  # Or json.loads(r.text) can also work
             new = self.data
             next_page = []
@@ -88,9 +93,9 @@ class APIRequestHandler:
 
             return self.data
         except requests.exceptions.RequestException as e:
-            return 1
+            return 0
         except ConnectionError:
-            return 1
+            return 0
 
     # Method to convert date format of ticket into good print
     def formatDates(self, updatedAt, createdAt):
